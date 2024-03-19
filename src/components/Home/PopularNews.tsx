6b1059224470554b7281f227f18f3faaf8772d2b
bharat <bhushansingh696@gmail.com>
2024-03-19 17:04:09 +0530

    feat: initialise project setup

---
+++ src/components/Home/PopularNews.tsx
@@ -0,0 +1,5 @@
+const PopularNews = () => {
+  return <div>Popular news</div>;
+};
+
+export default PopularNews;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import { Shell } from "@/components/Shell";
+
+const Home = () => {
+  return <Shell>Home Hi there</Shell>;
+};
+
+export default Home;
